0a4a8fdad340df6fd5c9620a2b2c3eeea36013d5
Jan Nijboer <jnijboer@schoolmaster.nl>
2022-09-27 16:00:05 +0200

    fix access permissions

--- access_policy.tf
+++ access_policy.tf
@@ -49,6 +49,45 @@ resource "azurerm_key_vault_access_policy" "terraform_sp_ap" {
   ]
 }
 
+resource "azurerm_key_vault_access_policy" "myuser_ap" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = var.tenant_id
+  object_id    = data.azuread_user.myuser
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Purge",
+    "WrapKey",
+    "UnwrapKey"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+}
+
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [azurerm_key_vault_access_policy.terraform_sp_ap, azurerm_key_vault_access_policy.myuser_ap]
   create_duration = "60s"
