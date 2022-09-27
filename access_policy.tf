3ee5628d03dafc5dfe0a8baa7c198ddaaa7c825a
Jan Nijboer <jnijboer@schoolmaster.nl>
2022-09-27 15:09:11 +0200

    new service principle

--- access_policy.tf
+++ access_policy.tf
@@ -1,5 +1,9 @@
-data "azuread_service_principal" "terraform_sp" {
+data "azuread_service_principal" "terraform_cloud_sp" {
   display_name = "terraform cloud"
+}
+
+data "azuread_service_principal" "terraform_sp" {
+  display_name = "terraform_service_principle"
 }
 
 data "azurerm_subscription" "subscription" {
@@ -10,7 +14,46 @@ data "azuread_user" "myuser" {
   user_principal_name = "jrnijboer_hotmail.com#EXT#@jrnijboerhotmail.onmicrosoft.com"
 }
 
-resource "azurerm_key_vault_access_policy" "terraform_ap" {
+resource "azurerm_key_vault_access_policy" "terraform_cloud_ap" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = var.tenant_id
+  object_id    = data.azuread_service_principal.terraform_cloud_sp.object_id
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Purge",
+    "WrapKey",
+    "UnwrapKey"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "terraform_sp_ap" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = var.tenant_id
   object_id    = data.azuread_service_principal.terraform_sp.object_id
@@ -87,7 +130,7 @@ resource "azurerm_key_vault_access_policy" "myuser_ap" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [azurerm_key_vault_access_policy.terraform_ap, azurerm_key_vault_access_policy.myuser_ap]
+  depends_on = [azurerm_key_vault_access_policy.terraform_cloud_ap, azurerm_key_vault_access_policy.terraform_sp_ap, azurerm_key_vault_access_policy.myuser_ap]
 
   create_duration = "60s"
 }
